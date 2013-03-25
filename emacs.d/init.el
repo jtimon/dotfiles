edc6d06aa0b2959ef976694df0e29b5925392977
jtimon <jtimonmv@gmail.com>
2013-03-25 19:17:01 +0100

    fixed custom-file. Starter-kit-lisp does strange things with C-left

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -8,10 +8,13 @@
 (when (not package-archive-contents)
   (package-refresh-contents))
 
-(defvar my-packages '(starter-kit starter-kit-lisp starter-kit-eshell starter-kit-js
+(defvar my-packages '(starter-kit starter-kit-eshell starter-kit-js
+                                  ;; starter-kit-lisp
                                   color-theme color-theme-solarized color-theme-sanityinc-solarized color-theme-sanityinc-tomorrow
-                                  undo-tree dtrt-indent coffee-mode
-                                  markdown-mode less-css-mode
+                                  ;; undo-tree
+                                  dtrt-indent
+                                  markdown-mode
+                                  less-css-mode
                                   auto-complete)
   "A list of packages to ensure are installed at launch.")
 
@@ -19,38 +22,12 @@
   (when (not (package-installed-p p))
     (package-install p)))
 
-(load "~/.emacs.d/custom.el")
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
+
+;; (load-theme 'sanityinc-solarized-dark)
+(load-theme 'sanityinc-tomorrow-night)
 
 ;; why not?
 (eshell)
-;; graaaaaaah! eshell doesn't respect eval-after-load for some reason:
-;; (with-current-buffer "*eshell*" (setq pcomplete-cycle-completions nil))
-;; (set-face-foreground 'eshell-prompt "turquoise")
 
-(load-theme 'sanityinc-solarized-dark)
-;; (load-theme 'sanityinc-tomorrow-night)
-
-;; (custom-set-variables
-;;  ;; custom-set-variables was added by Custom.
-;;  ;; If you edit it by hand, you could mess it up, so be careful.
-;;  ;; Your init file should contain only one such instance.
-;;  ;; If there is more than one, they won't work right.
-;;  '(custom-safe-themes (quote ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" default))))
-;; (custom-set-faces
-;;  ;; custom-set-faces was added by Custom.
-;;  ;; If you edit it by hand, you could mess it up, so be careful.
-;;  ;; Your init file should contain only one such instance.
-;;  ;; If there is more than one, they won't work right.
-;;  )
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" default))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
