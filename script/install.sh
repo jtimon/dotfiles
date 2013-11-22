696d0425c3aef6a8af040fb2352bfd679cb78c44
jtimon <jtimon@monetize.io>
2013-11-22 14:32:33 +0100

    Separate debian and ubuntu package lists

--- script/install.sh
+++ script/install.sh
@@ -78,17 +78,17 @@ done
 
 # Bootstrap a fresh Ubuntu install based on my app repos and deb lists.
 
-echo -n "Adding extra ppa-repositories ..."
-for repo in $(cat $lists/ppa_repos | tr '\n' ' '); do
-    echo -n "Adding repository $repo..."
-    sudo apt-add-repository ppa:$repo
-    echo "done"
-done
-
-echo -n "Updating package list ..."
-sudo apt-get update
-echo "done"
+# echo -n "Adding extra ppa-repositories ..."
+# for repo in $(cat $lists/ppa_repos | tr '\n' ' '); do
+#     echo -n "Adding repository $repo..."
+#     sudo apt-add-repository ppa:$repo
+#     echo "done"
+# done
+# 
+# echo -n "Updating package list ..."
+# sudo apt-get update
+# echo "done"
 
 echo -n "Installing packages via apt-get ..."
-sudo apt-get install -y $(cat $lists/debs | tr '\n' ' ')
+sudo apt-get install -y $(cat $lists/debs-debian | tr '\n' ' ')
 echo "done"
